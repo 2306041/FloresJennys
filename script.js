d244faf35fe3a0f90a7855edee1ef0c5c2c19f74
2306041 <2306041@utrodeo.edu.mx>
2025-09-21 16:33:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const initialMessage = document.querySelector('.initial-message');
     const mainContent = document.getElementById('main-content');
     const mainTitle = document.getElementById('main-title');
+    const finalMessage = document.getElementById('final-message');
+
+setTimeout(() => {
+    finalMessage.classList.remove('hidden');
+}, 4500);
 
     // 1. Mensaje inicial 3s
     setTimeout(() => {
         initialMessage.style.transition = 'opacity 2s ease-in-out';
         initialMessage.style.opacity = '0';
+
+
 
         // 2. Mostrar las flores después
         setTimeout(() => {
